b0b95cf5d9f68f9d413116049ab25c05c1051536
Durga Prasad Bokkisam <dbokkisam@nationsbenefits.com>
2022-02-03 20:53:32 +0530

    PPT

---
+++ Day9 Projects/Day9_program-Arthamatic Operators using OOPS/Day9_program-Arthamatic Operators using OOPS/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_program_Arthamatic_Operators_using_OOPS
+{
+    class MathTask
+    {
+        private int f;
+        private int g;
+
+        public void Readinput()
+        {
+            Console.WriteLine("Enter first number");
+            f = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            g = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public int Addnumbers()
+        {
+            return f + g;
+        }
+        public int Subtractnumbers()
+        {
+            return f - g;
+        }
+        public int Multiplynumbers()
+        {
+            return f * g;
+        }
+        public int Dividenumbers()
+        {
+            return f / g;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathTask mt = new MathTask();
+            mt.Readinput();
+            Console.WriteLine(mt.Addnumbers());
+            Console.WriteLine(mt.Subtractnumbers());
+            Console.WriteLine(mt.Multiplynumbers());
+            Console.WriteLine(mt.Dividenumbers());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day9 Projects/Day9_program1_factorial, factors and prime check/Day9_program1_factorial, factors and prime check/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9_program1_factorial__factors_and_prime_check
+{
+    class Mathsoperations
+    {
+        private int input;
+
+        public void Readinput()
+        {
+            Console.WriteLine("Enter the number");
+            input = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Factorial()
+        {
+            int fact = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+
+        public void Printfactors()
+        {
+            for (int i = 1; i <= input; i++)
+            {
+                if (input % i == 0)
+                    Console.WriteLine(i);
+            }
+        }
+
+        public bool Isprime()
+        {
+            int count = 0;
+            for (int i = 1; i <= input; i++)
+            {
+                if (input % i == 0)
+                    count++;
+
+            }
+            if (count == 2)
+                return true;
+            else
+                return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathsoperations obj = new Mathsoperations();
+            obj.Readinput();
+            obj.Factorial();
+            obj.Printfactors();
+            if (obj.Isprime())
+                Console.WriteLine("It is prime");
+            else
+                Console.WriteLine("It is not prime");
+
+            Console.ReadLine();
+
+        }
+    }
+}
